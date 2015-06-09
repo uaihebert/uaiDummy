54ac4339503aa58ffd246823f1de1ac57c94075d
hebert <contato@uaihebert.com>
2015-06-09 16:55:08 -0300

    updating config class name

--- src/main/java/com/uaihebert/uaidummy/config/DummyConfig.java
+++ src/main/java/com/uaihebert/uaidummy/config/DummyConfig.java
@@ -2,7 +2,7 @@ package com.uaihebert.uaidummy.config;
 
 import com.uaihebert.uaidummy.creditcard.DummyBaseCreditCard;
 
-public class UaiDummyConfig {
+public class DummyConfig {
 
     public static void setCreditCardExpirationDateMask(final String mask) {
         DummyBaseCreditCard.setDefaultExpirationDateMask(mask);
