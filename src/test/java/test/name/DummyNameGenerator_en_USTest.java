8021f25e5f19b1e8f8f5fc90aa2336a190429408
hebert <contato@uaihebert.com>
2015-07-07 14:14:10 -0300

    saving file for sasaki

--- src/test/java/test/name/DummyNameGenerator_en_USTest.java
+++ src/test/java/test/name/DummyNameGenerator_en_USTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-public class DummyNameGeneratorTest {
+public class DummyNameGenerator_en_USTest {
 
     @Test
     public void isGeneratingFirstName() {
